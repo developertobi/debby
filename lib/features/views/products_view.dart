4f6c0ac00955bd9afc9145c0cc9ed360f2b74ef7
developertobi <developertobi@gmail.com>
2022-02-26 13:22:31 +0100

    Implement load product without clicking a button

--- lib/features/views/products_view.dart
+++ lib/features/views/products_view.dart
@@ -1,17 +1,29 @@
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:debby/features/view_models/product_view_model.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-class ProductsView extends StatelessWidget {
+class ProductsView extends StatefulWidget {
   const ProductsView({Key? key}) : super(key: key);
+
+  @override
+  State<ProductsView> createState() => _ProductsViewState();
+}
+
+class _ProductsViewState extends State<ProductsView> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Provider.of<ProductViewModel>(context, listen: false).fetchAllProducts();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Consumer<ProductViewModel>(
       builder: (context, productModel, child) {
         return Scaffold(
-          appBar: AppBar(),
+          appBar: AppBar(title: const Text('Products')),
           body: productModel.productsLoading
               ? const Center(
                   child: CircularProgressIndicator(),
@@ -19,16 +31,51 @@ class ProductsView extends StatelessWidget {
               : ListView.separated(
                   itemCount: productModel.products.length,
                   itemBuilder: (context, index) {
-                    return ListTile(
-                      leading: CircleAvatar(
-                          child: Image.network(
-                              productModel.products[index].image[0])),
-                      title: Text(
-                          'Product name : ${productModel.products[index].name}'),
-                      subtitle: Text(
-                          'Store name: ${productModel.products[index].store.name}'),
-                      trailing: Text('${productModel.products[index].price}'),
+                    return StaggeredGrid.count(
+                      crossAxisCount: 4,
+                      mainAxisSpacing: 4,
+                      crossAxisSpacing: 4,
+                      children: [
+                        StaggeredGridTile.count(
+                          crossAxisCellCount: 2,
+                          mainAxisCellCount: 2,
+                          child: CircleAvatar(
+                              child: Image.network(
+                                  productModel.products[index].image[0])),
+                        ),
+                        const StaggeredGridTile.count(
+                          crossAxisCellCount: 2,
+                          mainAxisCellCount: 1,
+                          child: CircleAvatar(),
+                        ),
+                        const StaggeredGridTile.count(
+                          crossAxisCellCount: 1,
+                          mainAxisCellCount: 1,
+                          child: CircleAvatar(),
+                        ),
+                        const StaggeredGridTile.count(
+                          crossAxisCellCount: 1,
+                          mainAxisCellCount: 1,
+                          child: CircleAvatar(),
+                        ),
+                        const StaggeredGridTile.count(
+                          crossAxisCellCount: 4,
+                          mainAxisCellCount: 2,
+                          child: CircleAvatar(),
+                        ),
+                      ],
                     );
+                    ;
+// return ListTile(
+//                       leading: CircleAvatar(
+//                           child: Image.network(
+//                               productModel.products[index].image[0])),
+//                       title: Text(
+//                           'Product name : ${productModel.products[index].name}'),
+//                       subtitle: Text(
+//                           'Store name: ${productModel.products[index].store.name}'),
+//                       trailing: Text('${productModel.products[index].price}'),
+//                     );
                   },
                   separatorBuilder: (BuildContext context, int index) =>
                       const SizedBox(height: 10),
